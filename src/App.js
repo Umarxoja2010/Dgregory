fe4ac4ad9b142a099ba9a1db3f9ce507ee514cc5
Umarxoja2010 <umarxojamamarasulov@gmail.com>
2025-04-08 09:53:47 +0500

    togri

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 
-
 function App() {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === '/' || location.pathname === '/register';
+
   return (
     <div className="App">
-      <Header/>
-<Outlet/>
-<Footer/>
+      {!hideHeaderFooter && <Header />}
+      <Outlet />
+      {!hideHeaderFooter && <Footer />}
     </div>
   );
 }
